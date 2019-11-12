ad545ea6708051ef37e3bd244eb6ae129e65620b
Butch Cloninger <butch_cloninger@vfc.com>
2019-11-12 16:42:33 -0500

    initial commit

---
+++ bin/test/first.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('first',function(){
+    context('initial',function(){
+        it('should return 0',function(){
+            
+        })
+    })
+})
---
+++ test/first.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('first',function(){
+    context('initial',function(){
+        it('should return 0',function(){
+            var i = 0;
+            expect(i.to.equal(0))
+        })
+    })
+})
